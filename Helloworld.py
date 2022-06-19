8cd657d963185c6a0b04d19eeaf2d35aa7a31819
CheTmkGitHub <Chethan.workinfo@gmail.com>
2022-06-19 20:17:33 +0530

    here is 3rd

--- Helloworld.py
+++ Helloworld.py
@@ -8,5 +8,4 @@ num1 = num2
 num2 = temp
 print("value of num1: after swapping",num1)
 print("Value of num2: after swapping",num2)
-print("Here is first changes I have made ")
-print("Git is changed")
+print("Only one change")
